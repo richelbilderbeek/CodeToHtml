4494f4848ef92beb869ed7340b2ef0dc5ab1303c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-01 22:48:45 +0100

    EOD

--- codetohtmlreplacer.cpp
+++ codetohtmlreplacer.cpp
@@ -1,26 +1,9 @@
-//---------------------------------------------------------------------------
-/*
-CodeToHtml, converts C++ code to HTML
-Copyright (C) 2010-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/ToolCodeToHtml.htm
-//---------------------------------------------------------------------------
 #include "codetohtmlreplacer.h"
 
+#include <algorithm>
 #include <cassert>
+#include <sstream>
+#include <iostream>
 
 #include "codetohtmlreplacements.h"
 
@@ -142,12 +125,12 @@ void ribi::c2h::Replacer::Test() noexcept
   {
     std::stringstream s;
     s << "Number of C++ replacements: " << Replacer().GetReplacementsCpp().Get().size();
-    TRACE(s.str());
+    std::clog << s.str();
   }
   {
     std::stringstream s;
     s << "Number of .pro file replacements: " << Replacer().GetReplacementsPro().Get().size();
-    TRACE(s.str());
+    std::clog << s.str();
   }
 }
 #endif
